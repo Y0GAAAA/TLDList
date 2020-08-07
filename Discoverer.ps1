0b40ed1228642cffeaa3b2e9d1585a81bfe5f668
Y0GA <quainarthur@gmail.com>
2020-08-07 22:34:14 +0200

    Add Discoverer script

---
+++ Discoverer.ps1
@@ -0,0 +1,65 @@
+﻿ Param(
+     [Parameter(Mandatory=$true, Position=0, HelpMessage="base url without domain extension (example : http://google)")]
+     $baseurl
+   )
+
+Function IsReacheable($url) {
+
+    Try {
+
+        Invoke-WebRequest $url
+
+        return $TRUE
+
+   }
+   Catch 
+   {
+   
+        return $FALSE
+
+   }
+
+}
+
+Function SanitizeUrl($url) {
+
+    $url = $url.TrimEnd('.')
+
+    If (!$url.StartsWith("http")) {
+    
+        $url = ("http://" + $url)
+
+    }
+
+    return $url
+
+}
+
+Set-Variable fName -option Constant -value "list.bin"
+
+$fPath = [System.IO.Path]::Combine((Get-Location), $fName)
+
+$domains = [System.IO.File]::ReadAllLines($fPath)
+
+$baseurl = SanitizeUrl($baseurl)
+
+$MaxCount = $domains.Count
+$ProcessedCount = 0
+
+foreach($domain in $domains) {
+
+   $finalurl = ($baseurl + "." + $domain)
+
+   if (IsReacheable($finalurl)) {
+   
+        Write-Host $finalurl
+   
+   }
+
+   $ProcessedCount++
+
+   $Progress = ($ProcessedCount.ToString() + "/" + $MaxCount.ToString())
+
+   $host.UI.RawUI.WindowTitle = $Progress
+
+}
